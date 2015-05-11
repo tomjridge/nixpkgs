4a0d7bf83b05e424c109ad0c06962bfcc42a2793
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2015-05-11 20:37:50 +0100

    +

---
+++ ocamlenv.nix
@@ -0,0 +1,15 @@
+# based on https://nixos.org/wiki/Development_Environments clang.nix
+
+# nix-shell ocamlenv.nix -A ocamlEnv
+
+let
+     pkgs = import <nixpkgs> {};
+     stdenv = pkgs.stdenv;
+   in rec {
+     ocamlEnv = stdenv.mkDerivation rec {
+       name = "ocaml-env";
+       version = "1.1.1.1";
+       src = ./.;
+       buildInputs = [ pkgs.ocaml_4_02_1 pkgs.ocamlPackages_latest.findlib pkgs.ocamlPackages_latest.base64];
+     };
+   }
