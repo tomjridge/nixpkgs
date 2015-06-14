f41a61675058dc5fb36170397f76c69162f6f8db
Tom Ridge <tom.j.ridge@googlemail.com>
2015-06-14 16:09:51 +0100

    +

--- pkgs/applications/editors/eclipse/default.nix
+++ pkgs/applications/editors/eclipse/default.nix
@@ -114,6 +114,22 @@ in {
         };
   };
 
+  eclipse_scala_sdk_41 = buildEclipse {
+    name = "eclipse-scala_sdk-4.1.0";
+    description = "Eclipse IDE for Scala Developers";
+    src =
+      if stdenv.system == "x86_64-linux" then
+        fetchurl { # tested
+          url = http://downloads.typesafe.com/scalaide-pack/4.1.0-vfinal-luna-211-20150525/scala-SDK-4.1.0-vfinal-2.11-linux.gtk.x86_64.tar.gz;
+          sha256  = "d3e9efe1176d979dc0fe2a9457f70068b5a3fd23635a07bcd5639e2345897aca";
+        }
+      else
+        fetchurl { # untested
+          url = http://downloads.typesafe.com/scalaide-pack/4.0.0.vfinal-luna-211-20150305/scala-SDK-4.0.0-vfinal-2.11-linux.gtk.x86.tar.gz;
+          sha256 = "f422aea5903c97d212264a5a43c6ebc638aecbd4ce5e6078d92618725bc5d31e";
+        };
+  };
+
   eclipse_cpp_36 = buildEclipse {
     name = "eclipse-cpp-3.6.2";
     description = "Eclipse IDE for C/C++ Developers";
